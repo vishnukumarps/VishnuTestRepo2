f4d5ab3e1cc14654a3c59bf365a5ff9a5ecb2a4a
vishnukumarps <vishnukumarps2020@outlook.com>
2020-02-07 11:20:01 +0530

    M2 is Chnaged to M22

--- Models/Student.cs
+++ Models/Student.cs
@@ -16,6 +16,6 @@ namespace DemoProject.Models
         public int TotalMark { get; set; }
         public string Remark  { get; set; }
 
-        public string M2 { get; set; }
+        public string M22 { get; set; }
     }
 }
