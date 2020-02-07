7e4db6eb0be1b941df321ca840e4b0b378de58c8
vishnukumarps <vishnukumarps2020@outlook.com>
2020-02-07 10:21:20 +0530

    Student Model is Added

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoProject.Models
+{
+    public class Student
+    {
+        public  string InternalStudentId { get; set; }
+        public  string Name { get; set; }
+        public int M1 { get; set; }
+        public DateTime  Dob { get; set; }
+        public string  Dob2 { get; set; }
+        public long  EditedTime { get; set; }
+
+    }
+}
